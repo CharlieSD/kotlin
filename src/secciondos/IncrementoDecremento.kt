24838126af0de2af5dfa8218c629ecc99a6616ea
CharlieSD <carlos.espinoza@debite.com.mx>
2018-06-25 12:47:48 -0500

    Jerarquia de Operaciones

---
+++ src/secciondos/IncrementoDecremento.kt
@@ -0,0 +1,27 @@
+package secciondos
+/*
+   1.- ()
+   2.- ++x,--x (antes de variable)
+   3.- ^
+   4.- *,/
+   5.- +,-
+   6.- % mod
+   7.- =
+   8.- x++, x-- (despues de variable)
+ */
+
+fun main(args: Array<String>) {
+
+    var num1 = 10
+    val num2 = 5
+
+    val resultado : Int?
+
+    println(num1)
+
+    resultado = num1++ + num2
+
+    println(num1)
+
+    println("El resultado es $resultado")
+}
