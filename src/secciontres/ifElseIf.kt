caac6f50ad78e0348c13fcd35af784dc3b3e23d5
CharlieSD <carlos.espinoza@debite.com.mx>
2018-06-25 14:30:46 -0500

    If else if

---
+++ src/secciontres/ifElseIf.kt
@@ -0,0 +1,27 @@
+package secciontres
+
+fun main(args: Array<String>) {
+    println("Que calificación obtuvo en variables?")
+    val caliVariables : Int = readLine()!!.toInt()
+
+    println("Que calificación obtuvo en operacionesMatemáticas?")
+    val caliOperacionesMatematicas : Int = readLine()!!.toInt()
+
+    println("Que calificación obtuvo en operadoresLógicos?")
+    val caliOperadorLogico : Int = readLine()!!.toInt()
+
+    println("Que calificación obtuvo en condiciones?")
+    val caliCondiciones : Int = readLine()!!.toInt()
+
+    val promedio = (caliVariables + caliOperacionesMatematicas + caliOperadorLogico + caliCondiciones) / 4
+
+    if(promedio == 10){
+        println("Excelente, Felicidades!! tu calificación es $promedio")
+    } else if (promedio >= 9) {
+        println("Tu resultado es bueno, tu calificación es $promedio")
+    } else if (promedio >= 6 && promedio < 9) {
+        println("Tu resultado es suficiente, tu calificación es $promedio")
+    } else {
+        println("Tu resultado es insuficiente, tu calificación es $promedio")
+    }
+}
