39377f70c83c2a0eb457b3b296f8fdc220e74edd
CharlieSD <carlos.espinoza@debite.com.mx>
2018-06-25 17:52:49 -0500

    Ciclo While

---
+++ src/seccioncinco/While.kt
@@ -0,0 +1,11 @@
+package seccioncinco
+
+fun main(args: Array<String>) {
+    var i = 0
+
+    while (i < 5){
+        println("El valor actual de contetador es: $i")
+        i+=2
+    }
+    println("Ciclo while ha terminado")
+}
