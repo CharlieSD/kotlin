8cccf5a25d59711c5c4c06a32c6acdcf29bf0cc8
CharlieSD <carlos.espinoza@debite.com.mx>
2018-06-25 10:02:16 -0500

    Creando primeras variables

--- src/seccionuno/KotlinEjemplo.kt
+++ src/seccionuno/KotlinEjemplo.kt
@@ -1,3 +1,5 @@
+package seccionuno
+
 fun main (args: Array<String>) {
     println("Bienvenido")
     println("Este es el curso completo de Kotlin")
---
+++ src/seccionuno/Variables.kt
@@ -0,0 +1,20 @@
+package seccionuno
+
+fun main(args: Array<String>) {
+
+    var dobe = 2.1929817
+    var floatante = 23.14f
+
+    var x : Int = 2
+    val y : Int = 8
+    x = 9
+    var z : Int  = x + y
+
+    println("El valor de z es " + z)
+
+    var nombre : String  = "Charlie"
+    var edad : Int = 25
+    var ciudad : String = "Tlaxcala"
+
+    println("Mi nombre es $nombre, mi edad es $edad y vivo en $ciudad")
+}
