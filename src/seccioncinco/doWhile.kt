f363223e15510ec8e545a46e96675802cae6ff42
CharlieSD <carlos.espinoza@debite.com.mx>
2018-06-25 17:55:17 -0500

    Ciclo Do-While

---
+++ src/seccioncinco/doWhile.kt
@@ -0,0 +1,11 @@
+package seccioncinco
+
+fun main(args: Array<String>) {
+    var i = 1 // 7
+
+    do{
+        println("El valor actual de contetador es: $i")
+        i+=2
+    }while (i <= 5)
+    println("Ciclo while ha terminado")
+}
