fe73f4c4e7e80fec584df2e493226381365383ce
CharlieSD <carlos.espinoza@debite.com.mx>
2018-06-25 11:32:22 -0500

    Convertir Datos

--- src/seccionuno/ConvertirTipoDatos.kt
+++ src/seccionuno/ConvertirTipoDatos.kt
@@ -1,2 +1,24 @@
 package seccionuno
 
+fun main(args: Array<String>) {
+
+    var num1 : Int = 5
+    var num3 : Int = 10
+    var stringNumero : String = "25"
+
+    num1 = stringNumero.toInt()
+
+    println(num1)
+
+    var num2 : Float? = 12.5f
+
+    num2 = num3.toFloat()
+
+    println(num2)
+
+    var doble : Double
+    doble = num3.toDouble()
+    stringNumero = doble.toString()
+
+
+}
