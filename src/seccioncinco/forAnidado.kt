1b5f8c06a772ba498a3cdefe81df6389e8f99929
CharlieSD <carlos.espinoza@debite.com.mx>
2018-06-25 17:44:39 -0500

    Ciclo For anidado

--- src/seccioncinco/forAnidado.kt
+++ src/seccioncinco/forAnidado.kt
@@ -1,2 +1,12 @@
 package seccioncinco
 
+fun main(args: Array<String>) {
+    for (i in 0..5){
+        println("Contador es: $i")
+        for (j in 0..3){
+            println("Contador2 es: $j")
+        }
+        println("For anidado concluido")
+    }
+    println("El ciclo principal ha terminado")
+}
