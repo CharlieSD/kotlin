6679ef467c2b03d5165300867d9011c0b2625702
CharlieSD <carlos.espinoza@debite.com.mx>
2018-06-25 14:35:30 -0500

    If anidado

---
+++ src/secciontres/ifAnidado.kt
@@ -0,0 +1,17 @@
+package secciontres
+
+fun main(args: Array<String>) {
+
+    print("Ingrese la medida en metros:")
+    val medida : Double = readLine()!!.toDouble()
+
+    if(medida >= 1.00){
+        if (medida > 5.00){
+            println("La medida $medida sobrepasa los límites aeeptados")
+        } else {
+            println("La medida $medida es aceptable")
+        }
+    } else {
+        println("lo Siento, no es aceptable")
+    }
+}
