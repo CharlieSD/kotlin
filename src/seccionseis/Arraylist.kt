34a0f8124d211d5edb7edf4afe09e3367bb3a478
CharlieSD <carlos.espinoza@debite.com.mx>
2018-06-27 13:54:10 -0500

    Arraylist

--- src/seccionseis/Arraylist.kt
+++ src/seccionseis/Arraylist.kt
@@ -1,2 +1,36 @@
 package seccionseis
 
+fun main(args: Array<String>) {
+    val arralist = ArrayList<String>()
+
+    arralist.add("Uva")
+    arralist.add("Kiwi")
+    arralist.add("Mango")
+
+    println("El elemento en la posición tres es: " + arralist[2])
+
+    print("Todos los elementos son: ")
+    for (i in arralist){
+        println(i)
+    }
+
+    print("Todos los elementos por indice son: ")
+    for (i in 0..2){
+        println(arralist[i])
+    }
+
+    arralist[1] = ("Fresa")
+    arralist.set(0, "Sandia")
+
+    println(arralist)
+
+    if(arralist.contains("Sandia")){
+        println("se encontro")
+    }else{
+        println("No se encontro")
+    }
+
+    for (i in 0..arralist.size-1){
+        println(arralist[i])
+    }
+}
