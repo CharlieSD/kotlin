262a22f4929c526f84c73a60b4f989feddd4adde
CharlieSD <carlos.espinoza@debite.com.mx>
2018-06-28 14:59:23 -0500

    Seccion 8 - Interfaz

--- src/seccionocho/Enumerados.kt
+++ src/seccionocho/Enumerados.kt
@@ -1,2 +1,22 @@
 package seccionocho
 
+enum class Meses{
+    ENE, FEB, MAR, ABR, MAY, JUN, JUL, AGO, SEP, OCT, NOV, DIC
+}
+
+fun main(args: Array<String>) {
+    val sextoMes = Meses.JUN
+    val primerMes = Meses.ENE
+
+    println("Nombre: " + sextoMes.name)
+    println("Nombre: " + primerMes.name)
+    println("Posicion: " + sextoMes.ordinal)
+    println("Posicion: " + primerMes.ordinal)
+    println("Compara: " + sextoMes.compareTo(primerMes))
+    println("Compara: " + primerMes.compareTo(sextoMes))
+
+    for (items in Meses.values()){
+        println(items)
+    }
+
+}
